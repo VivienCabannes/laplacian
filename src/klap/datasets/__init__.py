0438d871fcc3887a3d11317df75d83ab353c4194
Vivien Cabannes <vivien.cabannes@gmail.com>
2023-03-22 10:11:42 -0400

    New visualization for spherical harmonics

--- src/klap/datasets/__init__.py
+++ src/klap/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .helper import meshgrid_3d
+
 from .synthetic import (
     concentric_circle,
     halfmoons,
--- src/klap/datasets/helper.py
+++ src/klap/datasets/helper.py
@@ -1,4 +1,31 @@
+"""
+Helpers functions
+
+@ Vivien Cabannes, 2023
+"""
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.special import sph_harm
+
+
+# Spherical helper functions
+def spherical_harmonic(x, num, return_eigenvalues=False, dtype=float):
+    out = np.zeros((len(x), num), dtype=dtype)
+    phi = np.arccos(x[:, 2])
+    theta = np.arctan2(x[:, 1], x[:, 0])
+    i = 0
+    eigenvalues = np.zeros(num)
+    for i in range(num):
+        deg = int(np.sqrt(i))
+        freq = i - deg**2 - deg
+        if dtype == float:
+            out[:, i] = np.real(sph_harm(freq, deg, theta, phi))
+        else:
+            out[:, i] = sph_harm(freq, deg, theta, phi)
+        eigenvalues[i] = deg * (deg + 1)
+    if return_eigenvalues:
+        return out, eigenvalues
+    return out
 
 
 def meshgrid_3d(n):
@@ -8,3 +35,15 @@ def meshgrid_3d(n):
     Y = np.outer(np.sin(theta), np.sin(phi))
     Z = np.outer(np.ones(n), np.cos(phi))
     return X, Y, Z
+
+
+def plot_sphere_surface(ax, X, Y, Z, f, cmap="RdBu", **kwargs):
+    cm = plt.get_cmap(cmap)(f.reshape(*X.shape))
+    ax.plot_surface(X, Y, Z, facecolors=cm, **kwargs)
+    ax.set_xlim([-0.8, 0.8])
+    ax.set_ylim([-0.8, 0.8])
+    ax.set_zlim([-0.8, 0.8])
+    ax.set_axis_off()
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    ax.set_zticklabels([])
