3b463411d42ebf842d83486a551f14856557e730
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-01-24 16:30:46 -0500

    Cleaning slurm config

--- grid/slurm_laucher.sh
+++ grid/slurm_laucher.sh
@@ -2,15 +2,11 @@
 
 # Logging configuration
 #SBATCH --job-name=eigenvals
-#SBATCH --output=/checkpoint/vivc/eigenvals-poly-%a-%t.out
-#SBATCH --error=/checkpoint/vivc/eigenvals-poly-%a-%t.err
+#SBATCH --output=eigenvals-poly-%a-%t.out
+#SBATCH --error=eigenvals-poly-%a-%t.err
 #SBATCH --mail-type=END
-#SBATCH --mail-user=vivc@meta.com
 
 # Job specification
-#SBATCH --partition=devlab
-#SBATCH --time=72:00:00
-#SBATCH --mem=256G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
